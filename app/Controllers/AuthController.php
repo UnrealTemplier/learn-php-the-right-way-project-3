53645a14263b134d325a30bab19c74020576e496
UnrealTemplier <jaimore.evseev@gmail.com>
2025-07-13 15:02:32 +0400

    Add registration form validation with apropriate middlewares (flash validation errors, old form data).

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\Entity\User;
+use App\Exception\ValidationException;
 use Doctrine\ORM\EntityManager;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
+use Valitron\Validator;
 
 class AuthController
 {
@@ -27,6 +29,22 @@ class AuthController
     public function register(Request $request, Response $response): Response
     {
         $data = $request->getParsedBody();
+
+        $v = new Validator($data);
+        $v->rule('required', ['name', 'email', 'password', 'confirmPassword']);
+        $v->rule('email', 'email');
+        $v->rule('equals', 'confirmPassword', 'password')->label('Confirm password');
+        $v->rule(
+            fn($field, $value, $params, $fields)
+                => !$this->entityManager
+                ->getRepository(User::class)
+                ->count(['email' => $value]),
+            "email",
+        )->message("User with the given email address already exists");
+
+        if (!$v->validate()) {
+            throw new ValidationException($v->errors());
+        }
 
         $user = new User();
         $user->setName($data['name']);
---
+++ app/Middleware/ValidationExceptionMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware;
+
+use App\Exception\ValidationException;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class ValidationExceptionMiddleware implements MiddlewareInterface
+{
+    public function __construct(private readonly ResponseFactoryInterface $responseFactory) {}
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        try {
+            return $handler->handle($request);
+        }
+        catch (ValidationException $e) {
+            $response = $this->responseFactory->createResponse();
+            $referer = $request->getServerParams()['HTTP_REFERER'];
+            $oldFormData = $request->getParsedBody();
+
+            $sensitiveFields = ['password', 'confirmPassword'];
+
+            $_SESSION['errors'] = $e->errors;
+            $_SESSION['old'] = array_diff_key($oldFormData, array_flip($sensitiveFields));
+
+            return $response->withHeader('Location', $referer)->withStatus(302);
+        }
+    }
+}
