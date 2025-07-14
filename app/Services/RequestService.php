fe91e4efe9af09d9ac97847a53da0e19f10e9b43
UnrealTemplier <jaimore.evseev@gmail.com>
2025-07-14 23:06:00 +0400

    Extract getting referer to RequestService class.

---
+++ app/Services/RequestService.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Contracts\SessionInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class RequestService
+{
+    public function __construct(private readonly SessionInterface $session) {}
+
+    public function getReferer(ServerRequestInterface $request): string
+    {
+        $referer = $request->getHeader('referer')[0] ?? '';
+        $sessionPreviousUrl = $this->session->get('previousUrl', '');
+
+        if (empty($referer)) {
+            $referer = $sessionPreviousUrl;
+        }
+
+        $refererHost = parse_url($referer, PHP_URL_HOST);
+        if ($refererHost !== $request->getUri()->getHost()) {
+            $referer = $sessionPreviousUrl;
+        }
+
+        return $referer;
+    }
+}
