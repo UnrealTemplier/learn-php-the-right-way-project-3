8ea0f88fcae9ec800082685353bec5d1b39fc1a6
UnrealTemplier <jaimore.evseev@gmail.com>
2025-07-18 16:47:28 +0400

    Add PopulateDbWithFakesController along with FakerPHP library and mbezhanov's FakerPHP providers collection.

---
+++ app/Controllers/PopulateDbWithFakesController.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Entity\Category;
+use Bezhanov\Faker\Provider\Commerce;
+use Doctrine\ORM\EntityManager;
+use Faker\Factory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class PopulateDbWithFakesController
+{
+    public function __construct(private readonly EntityManager $entityManager) {}
+
+    public function populateCategories(Request $request, Response $response): Response
+    {
+        $faker = Factory::create();
+        $faker->addProvider(new Commerce($faker));
+
+        $user = $request->getAttribute('user');
+
+        for ($i = 0; $i < 100; $i++) {
+            $category = new Category();
+            $category->setUser($user);
+            $category->setName($faker->category);
+            $this->entityManager->persist($category);
+        }
+
+        $this->entityManager->flush();
+
+        return $response;
+    }
+}
